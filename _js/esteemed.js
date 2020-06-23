44eb67a4115784d11282ea020782f0f198d50744
Silviu Serdaru <silviu@optasy.com>
2020-06-24 01:54:50 +0300

    EST-42: Sticky header

--- _js/esteemed.js
+++ _js/esteemed.js
@@ -1,2 +1,19 @@
 $(document).foundation();
 
+var site_nav = $('.w-nav');
+
+// Add shrink class to header when scrolling down.
+$(window)
+  .ready(function() {
+    if ($(window).scrollTop() >= site_nav.height()) {
+      site_nav.addClass('shrink');
+    }
+  })
+  .scroll(function() {
+    if ($(window).scrollTop() >= site_nav.height()) {
+      site_nav.addClass('shrink');
+    }
+    else {
+      site_nav.removeClass('shrink');
+    }
+  })
--- js/esteemed.js
+++ js/esteemed.js
@@ -14003,3 +14003,20 @@ var MotionUI = {
 
 $(document).foundation();
 
+var site_nav = $('.w-nav');
+
+// Add shrink class to header when scrolling down.
+$(window)
+  .ready(function() {
+    if ($(window).scrollTop() >= site_nav.height()) {
+      site_nav.addClass("shrink");
+    }
+  })
+  .scroll(function() {
+    if ($(window).scrollTop() >= 50) {
+      site_nav.addClass("shrink");
+    }
+    else {
+      site_nav.removeClass("shrink");
+    }
+  })

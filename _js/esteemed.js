33704e37b42c939ce67b3645b098d5962f5588b9
Silviu Serdaru <silviu@optasy.com>
2020-07-28 13:47:14 +0300

    EST-56: Hide header on sales-deck on mobile

--- _js/esteemed.js
+++ _js/esteemed.js
@@ -16,4 +16,20 @@ $(window)
     else {
       site_nav.removeClass('shrink');
     }
-  })
+  });
+
+// Hide navbar on mobile on mobile_hidden_navbar layout and show it only on scroll down.
+if ($(".page__mobile-nav-hidden").length) {
+  $(".w-nav").addClass('not-visible');
+
+  var lastScrollTop = 0;
+  $(window).scroll(function(event){
+    var ScrollTop = $(this).scrollTop();
+    if (ScrollTop > lastScrollTop){
+      $(".w-nav").removeClass('not-visible');
+    } else {
+      $(".w-nav").addClass('not-visible');
+    }
+    lastScrollTop = ScrollTop;
+  });
+}
